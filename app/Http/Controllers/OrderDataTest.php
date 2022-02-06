78250f68689b1835f0221c844d90cc3fcbde4aa6
Barakat Alrashdan <47255349+Barakat2019@users.noreply.github.com>
2022-02-06 18:42:53 +0200

    first

--- app/Http/Controllers/OrderDataTest.php
+++ app/Http/Controllers/OrderDataTest.php
@@ -9,7 +9,7 @@ class OrderDataTest extends Controller
 {
     public function get_data()
     {
-          $order_id = DB::connection('mysql')->table('wphpm_posts')->pluck('id');
+        $order_id = DB::connection('mysql')->table('wphpm_posts')->pluck('id');
         foreach ($order_id as $order) {
             return $order->ID;
         }
@@ -17,7 +17,8 @@ class OrderDataTest extends Controller
 
     public function get_customer_id()
     {
-          $transaction = DB::connection('mysql')->table('wphpm_postmeta')->where('meta_key', '_transaction_id')->get();
+        $transaction = DB::connection('mysql')->table('wphpm_postmeta')->where('meta_key', '_transaction_id')->get();
+        $paymentMethod = DB::connection('mysql')->table('wphpm_postmeta')->where('meta_key', '_payment_method')->get();
         $customer_id = DB::connection('mysql')->table('wphpm_postmeta')->where('meta_key', '_customer_user')->get();
     }
 }
--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -17,27 +17,26 @@ class OrdersSeeder extends Seeder
         $order_id = DB::connection('mysql')->table('wphpm_posts')->get();
         $customer_id = DB::connection('mysql')->table('wphpm_postmeta')->where('meta_key', '_customer_user')->get();
         $transaction_id = DB::connection('mysql')->table('wphpm_postmeta')->where('meta_key', '_transaction_id')->get();
-
+        $paymentMethod = DB::connection('mysql')->table('wphpm_postmeta')->where('meta_key', '_payment_method')->get();
 
         foreach ($order_id as $order) {
             foreach ($customer_id as $customer) {
                 foreach ($transaction_id as $trans) {
-                    DB::connection('mysql2')->table('orders')->insert([
-                        'customer_id' => $customer->meta_value,
-                        'created_by' => $order->post_author,
-                        'parent_id' => $order->post_parent,
-                        'status' => $order->post_status,
-                        'transaction_id' => $trans->meta_value,
-                        //'transaction_method' =>$trans->,
-                        'transaction_type' => 'A',
-                        'post_status' => '1',
-                        'refund_method' => '1',
-                        'refund_type' => 2,
-                        'payer_id' => 'tel',
-                        'shipping_fee' => 3.2,
-                        'cross_border_shipping_fee' => 12.3,
-                        'us_domestic_shipping_fee' => 1.2
-                    ]);
+                    foreach ($paymentMethod as $payment) {
+
+
+                        DB::connection('mysql2')->table('orders')->insert([
+                            'customer_id' => $customer->meta_value,
+                            'created_by' => $order->post_author,
+                            'parent_id' => $order->post_parent,
+                            'status' => $order->post_status,
+                            'transaction_id' => $trans->meta_value,
+                            'transaction_method' => $payment->meta_value,
+                            'refund_method'
+
+                        ]);
+                        break;
+                    }
                     break;
                 }
                 break;
